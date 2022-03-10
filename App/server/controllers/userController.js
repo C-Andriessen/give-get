fb73700386ac17f63e54e1b5f301fcf552a7bf6b
Collin Andriessen <candriessen@st.noorderpoort.nl>
2022-03-10 21:32:59 +0100

    email regex

--- App/server/controllers/userController.js
+++ App/server/controllers/userController.js
@@ -16,6 +16,16 @@ async function register(req,res) {
         errorMessage: "The passwords must match.",
       });
     }
+
+    const schoolDomainsRegex = ['noorderpoort\.nl'];
+    const emailRegexPattern = new RegExp(`^[a-zA-Z0-9_.+-]+@(?:(?:[a-zA-Z0-9-]+\.)?[a-zA-Z]+\.)?(${schoolDomainsRegex.join('|')})$`);
+    
+    if(!email.match(emailRegexPattern)) {
+      const schools = ['noorderpoort'];
+      return res.status(400).json({
+        errorMessage: `Je moet een geldig ${schools.join(', ').replace(/, ([^,]*)$/, ' of $1')} email gebruiken`,
+      });
+    }
   
     if (password.length < 6) {
       return res.status(400).json({
--- App/server/routers/testRouter.js
+++
@@ -1,7 +0,0 @@
-const testControllers = require("../controllers/test/testController");
-
-const router = require("express").Router();
-
-router.get("/", testControllers.testController);
-
-module.exports = router;
