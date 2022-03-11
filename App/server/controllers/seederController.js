40a48bb53122295f3314b862f1a3a2a1e1f05e0a
Collin Andriessen <candriessen@st.noorderpoort.nl>
2022-03-11 16:28:24 +0100

    user seeder

--- App/server/controllers/seederController.js
+++ App/server/controllers/seederController.js
@@ -1,4 +1,6 @@
 const Role = require('../models/role');
+const User = require('../models/user');
+const bcrypt = require("bcrypt");
 
 async function roleSeeder(req, res) {
     await Role.create({name: "BEHEERDER"})
@@ -8,4 +10,19 @@ async function roleSeeder(req, res) {
     res.end();
 }
 
-module.exports = {roleSeeder};
+async function userSeeder(req, res) {
+    const beheerder = await Role.findOne({name: "BEHEERDER"}).exec();
+    const docent = await Role.findOne({name: "DOCENT"}).exec();
+    const student = await Role.findOne({name: "STUDENT"}).exec();
+
+    await User.create({name: "Beheerder test user", email: "beheerder@giveandget.nl", passwordHash: bcrypt.hashSync("giveget", await bcrypt.genSalt(10)), active: true, role: beheerder._id});
+    await User.create({name: "Docent test user", email: "docent@giveandget.nl", passwordHash: bcrypt.hashSync("giveget", await bcrypt.genSalt(10)), active: true, role: docent._id});
+    await User.create({name: "Student test user", email: "student@giveandget.nl", passwordHash: bcrypt.hashSync("giveget", await bcrypt.genSalt(10)), active: true, role: student._id});
+
+    res.end();
+}
+
+module.exports = {
+    roleSeeder,
+    userSeeder
+};
--- App/server/routers/seederRouter.js
+++ App/server/routers/seederRouter.js
@@ -3,5 +3,6 @@ const seederController = require("../controllers/seederController.js");
 const router = require("express").Router();
 
 router.get('/role', seederController.roleSeeder);
+router.get('/user', seederController.userSeeder);
 
 module.exports = router;
