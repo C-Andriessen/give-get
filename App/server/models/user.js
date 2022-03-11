362ae249461aa3202d99599f38fcafca750dbfc8
Collin Andriessen <candriessen@st.noorderpoort.nl>
2022-03-11 14:23:32 +0100

    Role added to user

--- App/server/models/user.js
+++ App/server/models/user.js
@@ -5,7 +5,9 @@ const userSchema = new mongoose.Schema(
     name: {type: String, required: true},
     email: { type: String, required: true, unique: true},
     passwordHash: { type: String, required: true },
-    active: {type: Boolean}
+    active: {type: Boolean},
+    role: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 
+      "role" },
   },
   {
     timestamps: true,
