699c1649ef80c237db4a30a204ad38e65c9d3ad9
JouriR <jouri.roosjen@home.nl>
2022-06-10 14:11:37 +0200

    Added desktop navbar

--- App/client/src/layout/Header.js
+++ App/client/src/layout/Header.js
@@ -1,4 +1,5 @@
 import MobileMenu from "./MobileMenu";
+import DesktopMenu from "./DesktopMenu";
 
 import imgLogoFull from "../img/svg/LogoFull.svg";
 
@@ -7,6 +8,7 @@ export default function Header() {
     <header className="h-[55px] sm:h-[75px] lg:h-[95px] flex items-center justify-between mx-5">
       <img src={imgLogoFull} alt="Logo" className="h-[40px] sm:h-[60px]" />
       <MobileMenu classes="lg:hidden" />
+      <DesktopMenu classes="hidden uppercase font-bold text-xl text-black lg:block lg:flex lg:space-x-7 3xl:space-x-[90px]" />
     </header>
   );
 }
