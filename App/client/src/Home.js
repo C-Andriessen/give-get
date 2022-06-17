12b9851e1b1c208e8d213b91dee4cef6edb4a2f4
JouriR <jouri.roosjen@home.nl>
2022-06-17 09:39:16 +0200

    Added image & Deleted unused image

--- App/client/src/Home.js
+++ App/client/src/Home.js
@@ -2,7 +2,6 @@ import Header from "./layout/Header";
 import Feed from "./feed";
 import ButtonPrimary from "./common/ButtonPrimary";
 
-import imgLogoFull from "./img/svg/LogoFull.svg";
 import imgHands from "./img/Hands.png";
 import Pricing from "./components/Pricing";
 import HomeHero from "./components/HomeHero";
@@ -14,9 +13,9 @@ export default function Home() {
       <Header />
       <HomeHero />
       {/* Welcome section */}
-      <section className="grid grid-flow-row auto-rows-max text-center">
+      <section className="grid grid-flow-row text-center auto-rows-max">
         <div className="flex items-center justify-center place-content-center">
-          <h1 className="font-futura text-white font-extrabold text-4xl uppercase flex items-center flex-col md:flex-row sm:text-6xl">
+          <h1 className="flex flex-col items-center text-4xl font-extrabold text-white uppercase font-futura md:flex-row sm:text-6xl">
             WELKOM BIJ{" "}
             <span className="flex items-center italic">
               {" "}
@@ -33,7 +32,7 @@ export default function Home() {
       </section>
 
       {/* Trending section */}
-      <section className="grid auto-rows-max text-center place-items-center mt-40">
+      <section className="grid mt-40 text-center auto-rows-max place-items-center">
         <h2 className="font-futura font-bold text-xl mb-[30px] text-center md:text-3xl xl:mt-0 2xl:mt-10">
           ACTUEEL
         </h2>
@@ -44,19 +43,19 @@ export default function Home() {
       {/* Hero Section */}
       <section className="my-[50px]">
         <div className="relative">
-          <div className="absolute inset-x-0 bottom-0 h-1/2 bg-gray-100" />
+          <div className="absolute inset-x-0 bottom-0 bg-gray-100 h-1/2" />
           <div className="mx-auto">
             <div className="relative sm:overflow-hidden">
               <div className="absolute inset-0">
                 <img
-                  className="h-full w-full object-cover mix-blend-soft-light"
+                  className="object-cover w-full h-full mix-blend-soft-light"
                   src={imgHands}
                   alt="Reaching Hands"
                 />
                 <div className="absolute inset-0 bg-indigo-700 mix-blend-multiply" />
               </div>
               <div className="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
-                <h1 className="text-center tracking-tight">
+                <h1 className="tracking-tight text-center">
                   <span className="block text-white text-3xl sm:text-5xl lg:text-[70px] font-futura font-bold">
                     "THE MORE WE SHARE THE MORE WE HAVE"
                   </span>
