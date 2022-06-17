5b21a49e755aae15b88f7ee58cae38ecd4073f59
Collin Andriessen <candriessen@st.noorderpoort.nl>
2022-06-17 11:55:59 +0200

    feed item background

--- App/client/src/feed/FeedItem.js
+++ App/client/src/feed/FeedItem.js
@@ -1,7 +1,7 @@
 export default function FeedItem(props) {
   return (
-    <li className="col-span-1 flex flex-col text-center bg-paperTexture bg-white bg-blend-soft-light shadow divide-y divide-gray-200 w-[280px] h-[280px]">
-      <div className="flex flex-col justify-center flex-1 p-8">
+    <li className="col-span-1 flex flex-col text-center shadow w-72 h-72">
+      <div className="flex flex-col justify-center flex-1 p-8 bg-white/80 bg-paperTexture bg-blend-overlay">
         <h3 className="text-lg font-bold text-gray-900 font-roboto">
           Feed Item {props.number}!
         </h3>
